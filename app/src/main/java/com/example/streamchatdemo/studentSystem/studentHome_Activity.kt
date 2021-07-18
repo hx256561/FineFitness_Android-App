5005afdc7dfeeccf3d5a8e0d0caad900495cd4dc
liang <hx256561@gmail.com>
2021-07-18 17:40:48 +0800

    StudentHomeFragment可以連到profile，但是bottomNavigationView還有點問題

--- app/src/main/java/com/example/streamchatdemo/studentSystem/studentHome_Activity.kt
+++ app/src/main/java/com/example/streamchatdemo/studentSystem/studentHome_Activity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navArgs
 import com.example.streamchatdemo.R
 import com.example.streamchatdemo.muscleFlow.newLoginArgs
+import com.example.streamchatdemo.muscleFlow.newLoginDirections
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class studentHome_Activity : AppCompatActivity() {
 
@@ -20,9 +23,28 @@ class studentHome_Activity : AppCompatActivity() {
         setContentView(R.layout.activity_student_home)
 
         navController = findNavController(R.id.student_navHostFragment)
+        var studentBottomNav=findViewById<BottomNavigationView>(R.id.student_bottomNavigationView)
+        studentBottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+
 
         var action1=studentEmpty_fragmentDirections.actionStudentEmptyFragmentToStudentHomeFragment(args.chatUser)
         navController.navigate(action1)
 
+
+
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when (item.itemId) {
+            R.id.toProfile -> {
+                val action= studentHome_fragmentDirections.actionStudentHomeFragmentToStudentProfileFragment(args.chatUser)
+                navController.navigate(action)
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
     }
 }
--- app/src/main/java/com/example/streamchatdemo/studentSystem/studentProfile_fragment.kt
+++ app/src/main/java/com/example/streamchatdemo/studentSystem/studentProfile_fragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.navArgs
-import com.example.streamchatdemo.databinding.StudentHomeFragmentBinding
+import com.example.streamchatdemo.databinding.StudentProfileFragmentBinding
 
-class studentHome_fragment: Fragment() {
+class studentProfile_fragment:Fragment() {
 
-    private val args: studentHome_fragmentArgs by navArgs()
-    private var _binding:StudentHomeFragmentBinding?=null
+    private val args: studentProfile_fragmentArgs by navArgs()
+    private var _binding:StudentProfileFragmentBinding?=null
     private val binding get()=_binding!!
 
     override fun onCreateView(
@@ -20,7 +20,8 @@ class studentHome_fragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding= StudentHomeFragmentBinding.inflate(inflater,container,false)
+
+        _binding= StudentProfileFragmentBinding.inflate(inflater,container,false)
         return binding.root
     }
 }
