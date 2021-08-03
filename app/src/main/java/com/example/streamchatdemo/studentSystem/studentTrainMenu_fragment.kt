d174e852b517b267de39d8c55d1bba9fcfe37267
liang <hx256561@gmail.com>
2021-08-03 21:52:10 +0800

    新增菜單彈出視窗

--- app/src/main/java/com/example/streamchatdemo/studentSystem/studentTrainMenu_fragment.kt
+++ app/src/main/java/com/example/streamchatdemo/studentSystem/studentTrainMenu_fragment.kt
@@ -1,9 +1,14 @@
 package com.example.streamchatdemo.studentSystem
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.streamchatdemo.R
@@ -30,6 +35,10 @@ class studentTrainMenu_fragment: Fragment() {
             trainMenuAdapter.addTrainMenuItem(item1)
             trainMenuAdapter.addTrainMenuItem(item2)
 
+        binding.addTrainMenuItem.setOnClickListener {
+            addTrainItemDialog()
+        }
+
             setUpRecyclerView()
 
         return binding.root
@@ -38,6 +47,31 @@ class studentTrainMenu_fragment: Fragment() {
     private fun setUpRecyclerView(){
         binding.trainMenu.layoutManager=LinearLayoutManager(requireContext())
         binding.trainMenu.adapter=trainMenuAdapter
+    }
+
+    private fun addTrainItemDialog(){
+        var builder=AlertDialog.Builder(this.context)
+        var v=layoutInflater.inflate(R.layout.add_menu_item_layout,null)
+        builder.setView(v)
+        var input=v.findViewById<EditText>(R.id.add_item_input)
+        var finishBtn=v.findViewById<Button>(R.id.add_finish)
+        var cancelBtn=v.findViewById<Button>(R.id.add_cancel)
+        var dialog=builder.create()
+        dialog.show()
+
+        finishBtn.setOnClickListener {
+            //之後這裡要寫把菜單加到資料庫的code
+            val t=Toast.makeText(this.context,"Item added",Toast.LENGTH_SHORT)
+            t.setGravity(Gravity.CENTER_VERTICAL,0,0)
+            t.show()
+            dialog.dismiss()
+        }
+        cancelBtn.setOnClickListener {
+            val t=Toast.makeText(context,"Canceled",Toast.LENGTH_SHORT)
+            t.setGravity(Gravity.CENTER_VERTICAL,0,0)
+            t.show()
+            dialog.dismiss()
+        }
     }
 
 }
