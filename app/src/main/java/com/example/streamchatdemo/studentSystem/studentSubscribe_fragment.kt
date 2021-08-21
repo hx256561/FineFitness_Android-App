04819db97200827285923442682c572a1385469c
107306079chiu <107306079@g.nccu.edu.tw>
2021-08-22 01:54:24 +0800

    新增訂閱的fragment和layout(內容空白)

---
+++ app/src/main/java/com/example/streamchatdemo/studentSystem/studentSubscribe_fragment.kt
@@ -0,0 +1,22 @@
+package com.example.streamchatdemo.studentSystem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.streamchatdemo.databinding.StudentSubscribeFragmentBinding
+
+class studentSubscribe_fragment: Fragment() {
+    private var _binding:StudentSubscribeFragmentBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= StudentSubscribeFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
