970c3b1dc5ae31ebc28d38eb31ed6bf367cad672
107306079chiu <107306079@g.nccu.edu.tw>
2021-09-05 17:44:45 +0800

    新增become a coach頁裡上傳檔案功能和其他功能

--- app/src/main/java/com/example/streamchatdemo/studentSystem/studentProfileBeCoach_fragent.kt
+++ app/src/main/java/com/example/streamchatdemo/studentSystem/studentProfileBeCoach_fragent.kt
@@ -1,9 +1,12 @@
 package com.example.streamchatdemo.studentSystem
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.streamchatdemo.databinding.StudentProfileBcoachFragmentBinding
 
@@ -18,6 +21,23 @@ class studentProfileBeCoach_fragent: Fragment() {
     ): View? {
         _binding= StudentProfileBcoachFragmentBinding.inflate(inflater,container,false)
 
+        binding.btnApplyBCoach.setOnClickListener {
+            val t= Toast.makeText(this.context,"Success! Pending approval...", Toast.LENGTH_SHORT)
+            t.setGravity(Gravity.CENTER_VERTICAL,0,0)
+            t.show()
+        }
+
+        binding.btnChooseFile.setOnClickListener {
+            openGallery()
+        }
+
         return binding.root
+    }
+
+    private fun openGallery(){
+        // 謎之竟然成功打開檔案夾: https://camposha.info/android-examples/android-capture-pick-image/
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, 2)
     }
 }
